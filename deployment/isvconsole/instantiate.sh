4621050387a01de3998abad868c414215e64baf4
Robert Kleniewski <robert.kleniewski@gmail.com>
2021-09-25 16:44:12 +0200

    instantiate script

--- deployment/isvconsole/instantiate.sh
+++ deployment/isvconsole/instantiate.sh
@@ -1,7 +1,19 @@
 echo "Starting Instantiate script!"
 date
 
-echo "Value of APPNAME is \"${APPNAME}\""
-echo "APPCOLOR=${APPCOLOR}"
+export PARAPPNAME=$1
+export PARAPPVER=$2
+export PARTENANTNAME=$3
+
+
+export ID=`echo ${PARAPPNAME}-${PARAPPVER} | tr '[:upper:]' '[:lower:]'`
+export BUILD=${ID}-build
+
+echo "APPNAME=\"${APPNAME}\""
+echo "APPCOLOR=\"${APPCOLOR}\""
+
+oc new-app --image-stream=${BUILD} --name=${PARTENANTNAME} -e APPCOLOR=${APPCOLOR} -e APPNAME="${APPNAME}" 
+oc expose svc/${PARTENANTNAME}
+
 
 echo "Finished Instantiate script."
