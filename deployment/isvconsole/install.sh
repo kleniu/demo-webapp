fa2e29dfb0858c3ca046ed3dbcf88886eccfe0bf
Robert Kleniewski <robert.kleniewski@gmail.com>
2021-09-27 16:43:11 +0200

    changing install script

--- deployment/isvconsole/install.sh
+++ deployment/isvconsole/install.sh
@@ -3,19 +3,9 @@
 echo "## Installing application 'testapp'"
 export APPNAME=$1
 export APPVER=$2
-export OCPTOKEN=$3
-export OCPSERVER=$4
 
 cd `dirname $0`
-
-date
-
-## Login
-oc login --token=${OCPTOKEN} --server=${OCPSERVER}
-if [[ .$?. != .0. ]]; then
-    echo "## E: Cannot login to Openshift server ${OCPSERVER}"
-    exit 1
-fi
+echo "## Start " `date`
 
 ## Build images
 export ID=`echo ${APPNAME}-${APPVER} | tr '[:upper:]' '[:lower:]'`
@@ -23,6 +13,7 @@ export ID=`echo ${APPNAME}-${APPVER} | tr '[:upper:]' '[:lower:]'`
 if [[ .`oc get buildconfigs -n openshift | grep ${ID}-build`. != .. ]]; then
     echo "## I: build config ${ID}-build exists. Skipping creation."
 else
+    echo "#> oc new-build --strategy docker --name=${ID}-build  --binary=true -n openshift"
     oc new-build --strategy docker --name=${ID}-build  --binary=true -n openshift
     if [[ .$?. != .0. ]]; then
         echo "## E: Cannot create new build ${ID}-build"
@@ -30,11 +21,13 @@ else
     fi
 fi
 
-oc start-build ${ID}-build --from-dir="../../" --follow -n openshift
+echo "#> oc start-build ${ID}-build --from-dir='../../' --follow -n openshift"
+oc start-build ${ID}-build --from-dir='../../' --follow -n openshift
 if [[ .$?. != .0. ]]; then
     echo "## E: Cannot start build ${ID}-build"
     exit 1
 fi
 
-echo "## Done."
+echo "## Done " `date`
+
 exit 0
